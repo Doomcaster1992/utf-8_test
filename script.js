b83a8a0eb7da1bc620305bce98dff790fe458e21
Doomcaster1992 <105831499+Doomcaster1992@users.noreply.github.com>
2022-06-04 21:06:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Lern JavaScript' },
+      { text: 'Изучить Vue' },
+      { text: 'Создать что-нибудь классное' }
+    ]
+  }
+})     
